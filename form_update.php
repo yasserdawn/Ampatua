b6257525cd2bb175c4d7c91894f0bc9904488b99
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-04 04:57:17 -0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -13,23 +13,12 @@
     <head>  
       <link rel="stylesheet" type="text/css" href="css/style.css">
     </head>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<body>
-
-<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
-  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
     <div class="navbar">	
-        		<a href="index.php?page=home" id = "home">Home</a> class="w3-bar-item w3-button">Home</a>
-			<a href="product.php?page=product" id = "product">Show</a> class="w3-bar-item w3-button">Products</a>
-			<a href="categories.php?page=categories" id = "category">Category</a> class="w3-bar-item w3-button">Category</a>
-			<a href="form_create.php?page=create" id = "create">Create</a> 	 class="w3-bar-item w3-button">Create Products</a>
-					</div>
-				<div class="w3-teal">
-  <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>	
-					<h1>API Test</h1>
-  </div>
-</div>
+        		<a href="index.php?page=home" id = "home">Home</a> 
+			<a href="product.php?page=product" id = "product">Show</a> 
+			<a href="categories.php?page=categories" id = "category">Category</a> 
+			<a href="form_create.php?page=create" id = "create">Create</a> 	 
+					
 		<div id="box">		
 				<h1> Update Product </h1>
 </div>
@@ -52,25 +41,5 @@
 				</form>
 	</div>
 	</div>
-<?php
-}
-    ?>
 
-<script>
-function openLeftMenu() {
-  document.getElementById("leftMenu").style.display = "block";
-}
-
-function closeLeftMenu() {
-  document.getElementById("leftMenu").style.display = "none";
-}
-
-function openRightMenu() {
-  document.getElementById("rightMenu").style.display = "block";
-}
-
-function closeRightMenu() {
-  document.getElementById("rightMenu").style.display = "none";
-}
-</script>
 </html>
