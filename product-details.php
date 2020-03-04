88fa3db9775e9817a3ec65e5373dcbece0a42799
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-04 05:52:29 -0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -41,24 +41,6 @@ $value = $list;
 
             </table>
         </div>
-<script>
-function openLeftMenu() {
-  document.getElementById("leftMenu").style.display = "block";
-}
-
-function closeLeftMenu() {
-  document.getElementById("leftMenu").style.display = "none";
-}
-
-function openRightMenu() {
-  document.getElementById("rightMenu").style.display = "block";
-}
-
-function closeRightMenu() {
-  document.getElementById("rightMenu").style.display = "none";
-}
-
-</script>     
 </body>
 </html>
 
