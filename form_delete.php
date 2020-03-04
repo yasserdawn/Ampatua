a9b0d086023069ceb254acc5b1a7fcdec2034308
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-04 06:01:01 -0800

    Update form_delete.php

--- form_delete.php
+++ form_delete.php
@@ -1,7 +1,26 @@
   
-<form action="p_delete.php" method="POST">
-<h1> Delete</h1>
-<input type="text" name="id" placeholder="id"/>
-<input type="submit" name="submit" value="submit"/>
+<?php
+$id = $_GET['id'];
+$curl = curl_init();
 
-</form>
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "http://rdapi.herokuapp.com/product/delete.php",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS =>"{\n    \"id\" : \"$id\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "Content-Type: application/json",
+    "Content-Type: text/plain"
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+header("location:index.php?page=list")
