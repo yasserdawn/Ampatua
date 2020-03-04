b0d3114b696af65d27ec5aca5d1fc87a8615a1db
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-04 05:48:52 -0800

    Update form_create.php

--- form_create.php
+++ form_create.php
@@ -14,10 +14,10 @@
 
 <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
   <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
-  <a href="index.php?navigation=home" class="w3-bar-item w3-button">Home</a>
-  <a href="product.php?navigation=product" class="w3-bar-item w3-button">Products</a>
-  <a href="categories.php?navigation=categories" class="w3-bar-item w3-button">Category</a>
-  <a href="form_create.php?navigation=create" class="w3-bar-item w3-button">Create Products</a>
+  <a href="index.php?navi=home" class="w3-bar-item w3-button">Home</a>
+  <a href="product.php?navin=product" class="w3-bar-item w3-button">Products</a>
+  <a href="categories.php?navi=categories" class="w3-bar-item w3-button">Category</a>
+  <a href="form_create.php?navi=create" class="w3-bar-item w3-button">Create Products</a>
 </div>
 
 <div class="w3-teal">
