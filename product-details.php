427bdaf333a23f870d81e5502d2cd7c0c38c1a62
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 14:00:36 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -9,15 +9,24 @@ $value = $list;
 <html> 
     <head>  
 
-  <div class="navbar">
-        <a href="product.php?navigation=product">Product</a>
-        <a href="categories.php?navigation=categories">Category</a> 
-        <a href="form_create.php?navigation=create">Create</a> 
-    </div>
-<div class="header">
-  <h1>API Test</h1>
+  meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
+  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
+  <a href="index.php?navigation=home" class="w3-bar-item w3-button">Home</a>
+  <a href="product.php?navigation=product" class="w3-bar-item w3-button">Products</a>
+  <a href="categories.php?navigation=categories" class="w3-bar-item w3-button">Category</a>
+  <a href="form_create.php?navigation=create" class="w3-bar-item w3-button">Create Products</a>
+</div>
+
+<div class="w3-teal">
+  <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>
+ 
+  
+    <h1>API Test</h1>
   </div>
-<div id="box">
+</div>
 <h2>Product Details</h2>
 
 <table id="custom">
@@ -42,5 +51,22 @@ $value = $list;
         </div>
 </form>
 </body>
+<script>
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
+</script>
 </html>
 
