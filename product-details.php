7fe57b01ff4557a83617c78aeedb00e5d6d1a137
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 19:26:00 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -15,13 +15,7 @@ $value = $list;
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
-<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
-  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
-  <a href="index.php?navigation=home" class="w3-bar-item w3-button">Home</a>
-  <a href="index.php?navigation=product" class="w3-bar-item w3-button">Products</a>
-  <a href="index.php?navigation=categories" class="w3-bar-item w3-button">Category</a>
-  <a href="index.php?navigation=create" class="w3-bar-item w3-button">Create Products</a>
-</div>
+
 
 <div class="w3-teal">
   <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>
@@ -47,7 +41,7 @@ $value = $list;
                     <td><?php echo $value['price'];?></td>
                     <td><?php echo $value['category_id'];?></td>
 		<div id ="link">	
-			<td><a href="form_update.php?id=<?php echo $id ?>">Update</a></td>
+			<td><a href="?navi=formup&id=<?php echo $id ?>">Update</a></td>
         <td><a href="p_delete.php?id=<?php echo $id ?>">Delete</a></td>
     </tr>
 
