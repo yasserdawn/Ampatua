2200020047230914c4bffb786932a0fd920a9050
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 05:40:22 -0800

    Update product.php

--- product.php
+++ product.php
@@ -56,7 +56,7 @@ if(isset($_POST['search'])){
 foreach($list as $value){
     ?>
     <tr>
-        <<a href="product-details.php?id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
+        <a href="product-details.php?id=<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['description'];?></td>
         <td><?php echo $value['price'];?></td>
         <td><?php echo $value['category_name'];?></td>
