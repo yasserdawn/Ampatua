23f9f29595ab527eab629d4dd79e1bf9fa34e1d9
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 11:51:11 +0800

    Update product.php

--- product.php
+++ product.php
@@ -30,16 +30,11 @@ if(isset($_POST['search'])){
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 <body>
 
-<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
-  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
-  <a href="index.php?navigation=home" class="w3-bar-item w3-button">Home</a>
-  <a href="product.php?navigation=product" class="w3-bar-item w3-button">Products</a>
-  <a href="categories.php?navigation=categories" class="w3-bar-item w3-button">Category</a>
-  <a href="form_create.php?navigation=create" class="w3-bar-item w3-button">Create Products</a>
-</div>
 
-<div class="w3-teal">
-  <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>
+  <a href="index.php?navigation=home">Home</a>
+  <a href="product.php?navigation=product">Products</a>
+  <a href="categories.php?navigation=categories">Category</a>
+  <a href="form_create.php?navigation=create">Create Products</a>
 
     <h1>API Test</h1>
   </div>
@@ -71,22 +66,6 @@ foreach($list as $value){
 }
     ?>
 </table>
-<script>
-function openLeftMenu() {
-  document.getElementById("leftMenu").style.display = "block";
-}
-
-function closeLeftMenu() {
-  document.getElementById("leftMenu").style.display = "none";
-}
-
-function openRightMenu() {
-  document.getElementById("rightMenu").style.display = "block";
-}
-
-function closeRightMenu() {
-  document.getElementById("rightMenu").style.display = "none";
-}
-</script>
+	</html>
 
  
