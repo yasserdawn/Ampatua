572cc7bf81fd6d87482e27d261fe2731e4cd1f0d
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 05:43:56 -0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -46,7 +46,7 @@ $value = $list;
                     <td><?php echo $value['description'];?></td>
                     <td><?php echo $value['price'];?></td>
                     <td><?php echo $value['category_id'];?></td>
-		<div id ="link">
+		
 			<a href="form_update.php?id=<?php echo $id ?>">Update</a> or
 							<a href="p_delete.php?id=<?php echo $id ?>">Delete</a> </li>
 						</div>
