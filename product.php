bb403e0175c03ec065be1d2fdaf222146e5a0066
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 11:59:37 +0800

    Update product.php

--- product.php
+++ product.php
@@ -33,10 +33,10 @@ if(isset($_POST['search'])){
 <div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
   <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
 
-  <a href="index.php?navigation=home">Home</a> class="w3-bar-item w3-button">Home</a>
-  <a href="product.php?navigation=product">Products</a> class="w3-bar-item w3-button">Products</a>
-  <a href="categories.php?navigation=categories">Category</a> class="w3-bar-item w3-button">Category</a>
-  <a href="form_create.php?navigation=create">Create Products</a> class="w3-bar-item w3-button">Create Products</a>
+  <a href="index.php?navigation=home">Home</a>
+  <a href="product.php?navigation=product">Products</a>
+  <a href="categories.php?navigation=categories">Category</a>
+  <a href="form_create.php?navigation=create">Create Products</a>
 </div>
 <div class="w3-teal">
   <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>
