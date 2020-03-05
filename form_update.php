fc7adb859cace9bcf05fffb344d577c90747eafc
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 14:18:13 +0800

    Update form_update.php

--- form_update.php
+++ form_update.php
@@ -10,22 +10,29 @@
 	$category = $data2['records'];
 ?>
 <html> 
-    <head>  
-      <link rel="stylesheet" type="text/css" href="css/style.css">
-    </head>
-	<br>
-<div id="container">
-			<div id="content">
-					<div class="navbar">
-						<a href="index.php?navigation=home" id = "home">Home</a>
-						<a href="index.php?navigation=product" id = "product">Show</a>
-						<a href="index.php?navigation=categories" id = "category">Category</a>
-						<a href="index.php?navigation=create" id = "create">Create</a>
-					</div>
+    <<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
+  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
+  <a href="index.php?navigation=home" class="w3-bar-item w3-button">Home</a>
+  <a href="product.php?navigation=product" class="w3-bar-item w3-button">Products</a>
+  <a href="categories.php?navigation=categories" class="w3-bar-item w3-button">Category</a>
+  <a href="form_create.php?navigation=create" class="w3-bar-item w3-button">Create Products</a>
+</div>
+
+<div class="w3-teal">
+  <button class="w3-button w3-teal w3-xlarge w3-left" onclick="openLeftMenu()">&#9776;</button>
+ 
+  
+    <h1>API Test</h1>
+  </div>
+</div>
 					
 					
-		<div class="form-style-5">		
+		<div id="box">		
 				<h1> Update Product </h1>
+</div>
 				<form action="pro_update.php?id=<?php echo $id ?>" method="POST">
 					Product:<input type="text" name="name" value="<?php echo $result['name'];?>"/>
 					Description:<input type="text" name="description" value="<?php echo $result['description']; ?>"/>
@@ -43,7 +50,20 @@
 					<input type="submit" name="submit" value="submit"/>
 
 				</form>
-		</div>	
-			</div>	
-</div>	
+		<script>
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
 </html>
