0171cf82186e627b01af5d8c69401092a3f68237
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 14:31:04 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -46,10 +46,12 @@ $value = $list;
                     <td><?php echo $value['description'];?></td>
                     <td><?php echo $value['price'];?></td>
                     <td><?php echo $value['category_id'];?></td>
-</table>
+
+
                     <td id ="link"><a href="form_update.php?id=<?php echo $id ?>">Update</a> or
                     <a href="p_delete.php?id=<?php echo $id ?>">Delete</a></td>
                 </tr>
+</table>
 
 <script>
 function openLeftMenu() {
