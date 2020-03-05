a787fa4afffcde5024b7e64f6340aa943a9da5d9
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 11:47:13 +0800

    Update product-details.php

--- product-details.php
+++ product-details.php
@@ -8,13 +8,19 @@ $value = $list;
 ?>
 <html> 
     <head>  
-
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<body>
+<div class="w3-sidebar w3-bar-block w3-card w3-animate-left" style="display:none" id="leftMenu">
+  <button onclick="closeLeftMenu()" class="w3-bar-item w3-button w3-large">Close &times;</button>
   <div class="navbar">
-        <a href="product.php?navigation=product">Product</a>
-        <a href="categories.php?navigation=categories">Category</a>
-        <a href="form_create.php?navigation=create">Create</a>
+        <a href="product.php?navigation=product">Product</a> class="w3-bar-item w3-button">Product</a>
+        <a href="categories.php?navigation=categories">Category</a> class="w3-bar-item w3-button">Category</a>
+        <a href="form_create.php?navigation=create">Create</a> class="w3-bar-item w3-button">Create Products</a>
     </div>
-
+<div class="w3-teal">
+  <button class="w3-button w3-teal w3-xlarge w3-left" onc		lick="openLeftMenu()">&#9776;</button>
+<div class="header">
   <h1>API Test</h1>
   </div>
 <div id="box">
@@ -40,6 +46,26 @@ $value = $list;
 
             </table>
         </div>
+<input type="submit" name="submit" value="submit"/>
+<button type="Cancel" value="Cancel">Cancel</button>
+</form>
+<script>
+function openLeftMenu() {
+  document.getElementById("leftMenu").style.display = "block";
+}
+
+function closeLeftMenu() {
+  document.getElementById("leftMenu").style.display = "none";
+}
+
+function openRightMenu() {
+  document.getElementById("rightMenu").style.display = "block";
+}
+
+function closeRightMenu() {
+  document.getElementById("rightMenu").style.display = "none";
+}
+</script>
 </body>
 </html>
 
