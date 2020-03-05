e76ab9b4d2e4b0cf04d885200dac721b83ce0f83
yasserdawn <55270722+yasserdawn@users.noreply.github.com>
2020-03-05 20:19:37 +0800

    Update product.php

--- product.php
+++ product.php
@@ -57,7 +57,7 @@ foreach($list as $value){
     ?>
     <tr>
         <td><?php echo $value['id'];?></td>
-          <td><a href="product-details.php?navigation=details<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
+          <td><a href="product-details.php?navigation=product<?php echo $value['id'];?>"><?php echo $value['name'];?></a></td>
         <td><?php echo $value['price'];?></td>
     </tr>
 <?php
